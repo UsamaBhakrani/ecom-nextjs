16c306020da44b2196eb57dfd13340bf55fa3d35
Usama Bhakrani <usamabhakrani@gmail.com>
2024-08-16 20:44:25 +0500

    clear boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,3 @@
-import PostButton from "@/app/components/PostButton";
-import { createPost, getAllPosts } from "@/server/actions/posts";
-
 export default async function Home() {
-  const { error, success } = await getAllPosts();
-
-  if (error) throw new Error(error);
-  if (success)
-    return (
-      <main>
-        {success.map((post) => (
-          <div key={post.id}>{post.title}</div>
-        ))}
-        <form action={createPost}>
-          <input
-            className="border"
-            type="text"
-            placeholder="Title"
-            name="title"
-          />
-          <PostButton title="Submit Post" />
-        </form>
-      </main>
-    );
+  return <main>Hello</main>;
 }
--- server/index.ts
+++ server/index.ts
@@ -4,3 +4,4 @@ import * as schema from "@/server/schema";
 
 const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
 export const db = drizzle(sql, { schema, logger: true  });
+ 
