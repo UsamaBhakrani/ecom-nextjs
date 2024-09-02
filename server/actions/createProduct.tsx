85419c1af0de625bfa999c0ad1d1f2630441b294
Mohammed Usama <usamabhakrani@gmail.com>
2024-09-02 14:49:49 +0500

    Create Product Server action created

---
+++ server/actions/createProduct.tsx
@@ -0,0 +1,52 @@
+"use server";
+
+import { productsSchema } from "@/types/dashboardTypes";
+import { createSafeActionClient } from "next-safe-action";
+import { db } from "..";
+import { eq } from "drizzle-orm";
+import { products } from "../schema";
+
+export const safeActionClient = createSafeActionClient();
+
+export const createProduct = safeActionClient(
+  productsSchema,
+  async ({ id, title, description, price }) => {
+    try {
+      // Check if the product is already in the Db
+      if (id) {
+        const currentProduct = await db.query.products.findFirst({
+          where: eq(products.id, id),
+        });
+
+        // If Product not found return error message
+        if (!currentProduct) return { error: "Product not found" };
+
+        // If Product found in Db, update the product
+        const editedProduct = await db
+          .update(products)
+          .set({ description, price, title })
+          .where(eq(products.id, id))
+          .returning();
+
+        return {
+          success: `Product ${editedProduct[0].title} updated successfully`,
+        };
+      }
+
+      // If Product not found in Db, create a new product
+      if (!id) {
+        const newProduct = await db
+          .insert(products)
+          .values({
+            description,
+            price,
+            title,
+          })
+          .returning();
+        return { success: `Product ${newProduct[0].title} added successfully` };
+      }
+    } catch (error) {
+      return { error: JSON.stringify(error) };
+    }
+  }
+);
