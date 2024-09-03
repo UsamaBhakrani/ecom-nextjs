b3ea6501fa666f2c2ab706a3aab68f124c13a653
Usama Bhakrani <usamabhakrani@gmail.com>
2024-09-03 21:19:41 +0500

    setup tanstack table

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { db } from "@/server";
+import placeHolder from "@/public/placeholder_small.jpg";
+import { DataTable } from "@/components/product/DataTable";
+import { columns } from "@/components/product/Columns";
 
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+const ProductsPage = async () => {
+  const allProducts = await db.query.products.findMany({
+    orderBy: (products, { desc }) => [desc(products.id)],
+  });
+
+  if (!allProducts) throw new Error("No products found");
+
+  const dataTable = allProducts.map((product) => {
+    return {
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      variants: [],
+      image: placeHolder.src,
+    };
+  });
+
+  if (!dataTable) throw new Error("No data found");
+
+  return (
+    <div>
+      <DataTable data={dataTable} columns={columns} />
+    </div>
+  );
 };
 
 export default ProductsPage;
---
+++ components/product/Columns.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
+
+interface ProductColumn {
+  title: string;
+  image: string;
+  price: number;
+  id: number;
+  variants: any;
+}
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "variants",
+    header: "Variants",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        currency: "USD",
+        style: "currency",
+      }).format(price);
+      return <div className="font-md text-xs">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "image",
+    header: "Image",
+    cell: ({ row }) => {
+      const cellImage = row.getValue("image") as string;
+      const cellTitle = row.getValue("title") as string;
+      return (
+        <div>
+          <Image
+            src={cellImage}
+            alt={cellTitle}
+            className="w-12 h-12"
+            width={100}
+            height={100}
+          />
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    header: "Actions",
+  },
+];
