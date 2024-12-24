362866740ec20091ff6762b6dab7a37ce1450acb
Usama Bhakrani <usamabhakrani@gmail.com>
2024-12-24 20:20:59 +0500

    generateStaticParams

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -2,19 +2,42 @@ import { db } from "@/server";
 import { productVariants } from "@/server/schema";
 import { eq } from "drizzle-orm";
 
-const SingleProductPage = async ({
-  searchParams,
-  params,
-}: SearchParamProps) => {
-  const product = await db.query.productVariants.findMany({
-    where: eq(
-      productVariants.productID,
-      parseInt(searchParams?.productID as string)
-    ),
+export const generateStaticParams = async () => {
+  const data = await db.query.productVariants.findMany({
+    with: {
+      variantImages: true,
+      variantTags: true,
+      product: true,
+    },
+    orderBy: (productVariants, { desc }) => [desc(productVariants.id)],
   });
-  console.log(product);
 
-  return <div>SingleProductPage</div>;
+  if (data) {
+    const slugID = data.map((variant) => ({
+      slug: variant.id.toString(),
+    }));
+    return slugID;
+  }
+  return [];
+};
+
+const SingleProductPage = async ({ params }: { params: { slug: string } }) => {
+  const variant = await db.query.productVariants.findFirst({
+    where: eq(productVariants.id, parseInt(params.slug)),
+    with: { product: true },
+  });
+  return (
+    <main>
+      <section>
+        <div className="flex-1">
+          <h1>Images</h1>
+        </div>
+        <div className="flex gap-2 flex-col flex-1">
+          <h2>{variant?.product.description}</h2>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default SingleProductPage;
