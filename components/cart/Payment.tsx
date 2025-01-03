1b9ccdd90d28989b1aa9bef36e58e6bc15974e01
Usama Bhakrani <usamabhakrani@gmail.com>
2025-01-03 19:50:14 +0500

    setup stripe dark mode

--- components/cart/Payment.tsx
+++ components/cart/Payment.tsx
@@ -1,12 +1,15 @@
+"use client";
 import getStripe from "@/lib/getStripe";
 import { useCartStore } from "@/store";
 import { Elements } from "@stripe/react-stripe-js";
 import { motion } from "framer-motion";
 import PaymentForm from "./PaymentForm";
+import { useTheme } from "next-themes";
 
 const stripe = getStripe();
 const Payment = () => {
   const { cart } = useCartStore();
+  const { theme } = useTheme();
   const totalPrice = cart.reduce((acc, item) => {
     return acc + item.price * item.variant.quantity;
   }, 0);
@@ -14,7 +17,12 @@ const Payment = () => {
     <motion.div className="mx-auto max-w-2xl">
       <Elements
         stripe={stripe}
-        options={{ mode: "payment", currency: "usd", amount: totalPrice * 100 }}
+        options={{
+          mode: "payment",
+          currency: "usd",
+          amount: totalPrice * 100,
+          appearance: { theme: theme === "dark" ? "night" : "flat" },
+        }}
       >
         <PaymentForm totalPrice={totalPrice} />
       </Elements>
